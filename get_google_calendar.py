adc132cf2df497594ab16483d71bf11f6f673fef
Ryan Dean <ryan@mobalyticshq.com>
2017-12-04 08:07:48 -0800

    removed env files

--- get_google_calendar.py
+++ get_google_calendar.py
@@ -11,12 +11,6 @@ from apcontent import alarmpi_content
 
 import datetime
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-print flags
 class google_calendar(alarmpi_content):
   def get_credentials():
     """Gets valid user credentials from storage.
@@ -35,11 +29,11 @@ class google_calendar(alarmpi_content):
                                    'alarmpi-calendar-service.json')
 
     store = Storage(credential_path)
-    print('here', store)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
+        print(flags)
         if flags:
             credentials = tools.run_flow(flow, store, flags)
         else: # Needed only for compatibility with Python 2.6
