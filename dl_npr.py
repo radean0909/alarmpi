4bc4540c67257fab2f15f039f8f87a31e37bef46
Ryan Dean <ryan@mobalyticshq.com>
2017-12-04 19:24:27 -0800

    updated dler

--- dl_npr.py
+++ dl_npr.py
@@ -2,15 +2,14 @@
 import subprocess
 import feedparser
 
-def dl_npr(ramdrive='/mnt/ram/')
+def dl_npr(ramdrive='/mnt/ram/'):
       rss_url = 'http://www.npr.org/rss/podcast.php?id=500005'
       rss = feedparser.parse(rss_url)
       rval = True
 
       media_url = rss.entries[0].links[0].href
       print(media_url)
-      head = self.sconfig['head']
-      tail = self.sconfig['tail']
+      head = 'wget -q -U Mozilla'
       st = head + ' -O ' + ramdrive + 'npr.mp3 ' + media_url
       print(st)
       print subprocess.call (st, shell=True)
