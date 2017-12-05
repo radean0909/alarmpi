56e0d2860239e70a7fce2be5f8eec1ae391901f8
Ryan Dean <ryan@mobalyticshq.com>
2017-12-05 08:17:03 -0800

    updated alarm to include wakeup sound

--- get_npr.py
+++ get_npr.py
@@ -6,7 +6,7 @@ from apcontent import alarmpi_content
 
 class npr(alarmpi_content):
   def begin(self, ramdrive='/mnt/ram/'):
-    play = self.sconfig['player'] + ' /alarmpi/wakeup.mp3'
+    play = self.sconfig['player'] + ' ~/alarmpi/wakeup.mp3'
     print subprocess.call (play, shell=True)
     return True
 
