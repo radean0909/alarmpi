014ec37fbd2422453ebfa1cf4b9b4f7b87cdb907
Ryan Dean <ryan@mobalyticshq.com>
2017-12-05 10:27:35 -0800

    updated volume control

--- dl_npr.py
+++ dl_npr.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import subprocess
 import feedparser
-import alsaaudio
 
 
 def dl_npr(ramdrive='/mnt/ram/'):
@@ -12,9 +11,4 @@ def dl_npr(ramdrive='/mnt/ram/'):
       media_url = rss.entries[0].links[0].href
       head = 'wget -q -U Mozilla'
       st = head + ' -O ' + ramdrive + 'npr.mp3 ' + media_url
-      m = alsaaudio.Mixer(alsaaudio.mixers[0]) # alsaaudio.mixers = ["PCM"] for me.
-      print alsaaudio.mixers
-      print m.getvolume()
-      m.setvolume(80, 'MIXER_CHANNEL_ALL') # Or whatever
-      print m.getvolume()
       print subprocess.call (st, shell=True)
--- get_npr.py
+++ get_npr.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import subprocess
 import feedparser
+import alsaaudio
 
 from apcontent import alarmpi_content
 
 class npr(alarmpi_content):
   def begin(self, ramdrive='/mnt/ram/'):
+      m = alsaaudio.Mixer(alsaaudio.mixers[0]) # alsaaudio.mixers = ["PCM"] for me.
+      print alsaaudio.mixers
+      print m.getvolume()
+      m.setvolume(80, 'MIXER_CHANNEL_ALL') # Or whatever
+      print m.getvolume()
     play = self.sconfig['player'] + ' ~/alarmpi/wakeup.mp3'
     print subprocess.call (play, shell=True)
     return True
